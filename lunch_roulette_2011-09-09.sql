edc0ac5ec9f12103c36431a88514b833467da301
Corey Menscher <corey@findings.com>
2011-09-09 15:51:00 -0400

    MySQL creation scripts

---
+++ lunch_roulette_2011-09-09.sql
@@ -0,0 +1,59 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 3408
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: ec2-50-16-167-75.compute-1.amazonaws.com (MySQL 5.1.52)
+# Database: lunch_roulette
+# Generation Time: 2011-09-09 15:45:55 -0400
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table Lunches
+# ------------------------------------------------------------
+
+CREATE TABLE `Lunches` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `lunch_id` int(11) DEFAULT NULL,
+  `person_id` int(11) DEFAULT NULL,
+  `lunch_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table People
+# ------------------------------------------------------------
+
+CREATE TABLE `People` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(512) DEFAULT NULL,
+  `email` varchar(512) DEFAULT NULL,
+  `avail_1` tinyint(1) DEFAULT NULL,
+  `avail_2` tinyint(1) DEFAULT NULL,
+  `avail_3` int(11) DEFAULT NULL,
+  `avail_4` int(11) DEFAULT NULL,
+  `last_updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
