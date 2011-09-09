fe968528dcb21338b6925721201a70cd6f5d56a7
Mario Menti <mmenti@gmail.com>
2011-09-09 17:24:10 -0400

    removed some shit we don't need anymore

--- py/get_data.py
+++ py/get_data.py
@@ -28,25 +28,6 @@ class SimpleCRUD:
     self.gd_client.password = password
     self.gd_client.source = 'betaworks lunch roulette'
     self.gd_client.ProgrammaticLogin()
-    self.curr_key = ''
-    self.curr_wksht_id = ''
-    self.list_feed = None
-
-  def _PrintFeed(self, feed):
-    for i, entry in enumerate(feed.entry):
-      if isinstance(feed, gdata.spreadsheet.SpreadsheetsCellsFeed):
-        print '%s %s\n' % (entry.title.text, entry.content.text)
-      elif isinstance(feed, gdata.spreadsheet.SpreadsheetsListFeed):
-        print '%s %s %s' % (i, entry.title.text, entry.content.text)
-        # Print this row's value for each column (the custom dictionary is
-        # built from the gsx: elements in the entry.) See the description of
-        # gsx elements in the protocol guide.
-        print 'Contents:'
-        for key in entry.custom:
-          print '  %s: %s' % (key, entry.custom[key].text)
-        print '\n',
-      else:
-        print '%s %s\n' % (i, entry.title.text)
 
   def _ProcessData(self, feed):
     db = MySQLdb.connect("localhost","lunchy","lunchroulette","lunch_roulette" )
@@ -69,16 +50,12 @@ class SimpleCRUD:
 
   def Run(self):
 
-    # Get the list of spreadsheets
     feed = self.gd_client.GetSpreadsheetsFeed()
-    # self._PrintFeed(feed)
     # assume doc index=0
     id_parts = feed.entry[0].id.text.split('/')
     doc_key = id_parts[len(id_parts) - 1]
   
-    # Get the list of worksheets
     feed = self.gd_client.GetWorksheetsFeed(doc_key)
-    # self._PrintFeed(feed)
     # assume worksheet index=0
     id_parts = feed.entry[0].id.text.split('/')
     ws_key = id_parts[len(id_parts) - 1]
