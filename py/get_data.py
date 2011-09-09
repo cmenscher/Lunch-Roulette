32ee9f590e4ac7b556e705e770cff6680bc6c472
Mario Menti <mmenti@gmail.com>
2011-09-09 17:15:53 -0400

    now actually update the People table..

--- py/get_data.py
+++ py/get_data.py
@@ -18,6 +18,7 @@ import getopt
 import sys
 import string
 import MySQLdb
+import datetime
 
 class SimpleCRUD:
 
@@ -51,10 +52,19 @@ class SimpleCRUD:
     db = MySQLdb.connect("localhost","lunchy","lunchroulette","lunch_roulette" )
     cursor = db.cursor()
     for i, entry in enumerate(feed.entry):
-      print map(lambda e: (e[0],e[1].text), entry.custom.items())
+      rowdata =  map(lambda e: (e[1].text), entry.custom.items())
+      l_updated = datetime.datetime.strptime(rowdata[0],"%d/%m/%Y %H:%M:%S")
+      use_av_1 = 0
+      use_av_2 = 0
+      use_av_3 = 0
+      use_av_4 = 0
+      if rowdata[1].find('12:00') != -1:  use_av_1 = 1
+      if rowdata[1].find('12:30') != -1:  use_av_2 = 1
+      if rowdata[1].find('1:00') != -1:  use_av_3 = 1
+      if rowdata[1].find('1:30') != -1:  use_av_4 = 1
+      mysql_update_qry = "update People set last_updated = '%s', avail_1 = %s, avail_2 = %s, avail_3 = %s, avail_4 = %s where email = '%s'" % (l_updated, use_av_1, use_av_2, use_av_3, use_av_4, rowdata[2])
+      cursor.execute(mysql_update_qry)
 
-    # cursor.execute("SELECT VERSION()")
-    # data = cursor.fetchone()
     db.close()
 
   def Run(self):
